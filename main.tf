2be5ed400b9906541e37edcfa1a50b3186391639
tryan225 <tryan225@icloud.com>
2020-05-19 11:59:40 -0400

    testing shipyard run

--- main.tf
+++ main.tf
@@ -8,17 +8,18 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "lab_server" {
-  ami                         = "ami-014aa75ce392856d7"
-  instance_type               = "t2.micro"
-  key_name                    =  "tyler-us-east-1"
+  ami                         = "ami-0122f91fd9e5c346a"
+  instance_type               = "t2.medium"
+  key_name                    = "tyler-us-east-1"
   vpc_security_group_ids      = [aws_security_group.allow_http_docs_site.id]
   associate_public_ip_address = true
   tags                        = {
-    Name = "HelloWorld"
+    Name = "support-lab-test"
   } 
   user_data                   = <<EOF
     #! /bin/bash
     sudo curl https://shipyard.run/install | bash
+    shipyard run github.com/shipyard-run/blueprints//consul-docker
   EOF
 }
 
